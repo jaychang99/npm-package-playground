014291ebeeb926d03824554f0b59a610f818a144
jaychang99 <jaychang99@gmail.com>
2024-04-06 08:44:37 +0900

    feat: test utility functions

--- tests/my-first-npm-package.js
+++ tests/my-first-npm-package.js
@@ -1,3 +1,12 @@
-import { printMsg } from '@jaychang99/my-first-npm-package';
+import { add, divide, printMsg } from '@jaychang99/my-first-npm-package';
 
 console.log(printMsg());
+
+console.log(add(1, 2));
+
+
+try {
+  console.log(divide(6, 0));
+} catch (e) {
+  console.error(e.message);
+}
